5bb6c84b0cf2dff15b7a7538f68c16b6af4976f1
James <tytechortz@gmail.com>
2018-11-01 20:21:26 -0600

    finishes App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,38 +1,40 @@
 import React, { Component } from 'react';
-import WeatherData from '/WeatherData/WeatherData';
-
+import WeatherData from './WeatherData/WeatherData';
 
 class App extends Component {
   constructor(){
     super();
-
+    
     this.state = {
       weather: []
     }
   }
-
-getWeather = async () {
+  
+getWeather = async () => {
 
   try{
     const weather = await fetch('http://api.openweathermap.org/data/2.5/weather?zip=80206,us&APPID=d177ecaf5436da5c14f410fb73b64b4d');
-    const WeatherData = await weather.json();
+    const weatherJson = await weather.json();
     return weatherJson
   } catch(err){
     return err
   }
-}  
+}
 
 componentDidMount(){
-  this.getWeather().then((data => {
+  this.getWeather().then((data) => {
     console.log(data, 'this is data');
     this.setState({weather: data});
 
-  }))
+  }).catch((err) => {
+    console.log(err);
+
+  });
 }
   render() {
     return (
-      <div className="App">
-       
+      <div>
+        <WeatherData weather={this.state.weather} />
       </div>
     );
   }
