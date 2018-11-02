03e56e4e0411f7c53e06b301a3cf40f805c76f11
James <tytechortz@gmail.com>
2018-11-01 20:14:42 -0600

    adds api call

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -11,7 +11,17 @@ class App extends Component {
     }
   }
 
-  
+getWeather = async () {
+
+  try{
+    const weather = await fetch('http://api.openweathermap.org/data/2.5/weather?zip=80206,us&APPID=d177ecaf5436da5c14f410fb73b64b4d');
+    const WeatherData = await weather.json();
+    return weatherJson
+  } catch(err){
+    return err
+  }
+}  
+
   render() {
     return (
       <div className="App">
