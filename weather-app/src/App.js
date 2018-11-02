3d4fb0975ae090c326036b94874e2cd1fd6ef96f
James <tytechortz@gmail.com>
2018-11-01 20:12:12 -0600

    sets up app.js class

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import WeatherData from '/WeatherData/WeatherData';
 
-import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      weather: []
+    }
+  }
+
+  
   render() {
     return (
       <div className="App">
